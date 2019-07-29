030b1e439476b4122e9882b393ddc0e351491ec9
lelanthran <lelanthran@gmail.com>
2019-07-29 21:06:42 +0200

    Added new empty files to this project.

---
+++ src/src/babylon_cli.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+
+int main (void)
+{
+   int ret = EXIT_FAILURE;
+   printf ("Starting babylon processing\n");
+
+   return ret;
+}
+
---
+++ src/src/babylon_text.h
@@ -0,0 +1,17 @@
+
+#ifndef H_BABYLON_TEXT
+#define H_BABYLON_TEXT
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef __cplusplus
+};
+#endif
+
+#endif
+
