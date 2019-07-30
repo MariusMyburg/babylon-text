6f0e066279b6cf65c87e7e2766a02918ff606158
lelanthran <lelanthran@gmail.com>
2019-07-30 06:46:07 +0200

    WIP. Small start on data structures. Not very sure yet about lexing, parsing. Probably best to do both in single step.

--- src/src/babylon_text.c
+++ src/src/babylon_text.c
@@ -1,3 +1,36 @@
 
 #include "babylon_text.h"
 
+#include "ds_array.h"
+#include "ds_hmap.h"
+
+
+enum node_type_t {
+   node_NODE,
+   node_VALUE
+};
+
+typedef struct node_t node_t;
+
+struct node_t {
+   // Token location
+   char *filename;
+   size_t line;
+   size_t charpos;
+
+   // The actual token.
+   // Each node is either a pointer to another tree or a value. If it's a
+   // NODE type then 'text' contains the tag value, otherwise 'text'
+   // contains the text of the token and the remaining fields are ignored.
+   enum node_type_t type;
+   char *text;
+   ds_hmap_t *hmap;
+   size_t nnodes;
+   void **nodes;
+};
+
+struct babylon_text_t {
+   size_t nnodes;
+   void **nodes;
+};
+
--- src/src/babylon_text.h
+++ src/src/babylon_text.h
@@ -2,10 +2,29 @@
 #ifndef H_BABYLON_TEXT
 #define H_BABYLON_TEXT
 
+#include <stdbool.h>
+#include <stdio.h>
+
+
+typedef struct babylon_text_t babylon_text_t;
+typedef struct babylon_macro_t babylon_macro_t;
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
+   babylon_macro_t *babylon_macro_read (const char *filename);
+   void babylon_macro_del (babylon_macro_t *m);
+
+
+   babylon_text_t *babylon_text_new (void);
+   babylon_text_t *babylon_text_transform (babylon_text_t *src,
+                                           const babylon_macro_t *m);
+
+   void babylon_text_del (babylon_text_t *b);
+
+   bool babylon_text_read (babylon_text_t *b, const char *filename);
+   bool babylon_text_write (babylon_text_t *b, FILE *outf);
 
 
 
